70e69b83f472638b511e30ede188d08f0ee2d651
raguraman <raguraman.mps6066@gmail.com>
2024-08-30 10:36:17 +0530

    second commit

--- books_pydantic.py
+++ books_pydantic.py
@@ -1,6 +1,7 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI,Path,Query,HTTPException
 from pydantic import BaseModel,Field
+from starlette import status
 
 app=FastAPI()
 
@@ -10,13 +11,15 @@ class Book:
     author:str
     description:str
     rating:int
+    published_date:int
 
-    def __init__(self, id, title,author, description, rating):
+    def __init__(self, id, title,author, description, rating,published_date):
         self.id=id
         self.title=title
         self.author=author
         self.description=description
         self.rating=rating
+        self.published_date=published_date
 
 
 # validation
@@ -26,6 +29,7 @@ class BookRequest(BaseModel):
     author:str=Field(min_length=1)
     description:str=Field(min_length=1,max_length=100)
     rating:int=Field(gt=0,lt=6)
+    published_date:int=Field(gt=1999,lt=2031)
 
     class Config:
         json_schema_extra={
@@ -33,28 +37,58 @@ class BookRequest(BaseModel):
                 "title":"a new book",
                 "author":"codingwithragu",
                 "description":"a new description",
-                "rating":5
+                "rating":5,
+                "publised_date":2029
             }
         }
 
 BOOKS=[
-    Book(1,"computer science pro","code with roby","very nice book",5),
-    Book(2,"Be fast with fastapi","code with roby","this is great book",5),
-    Book(3,"Master end point","code with roby","very nice book",4),
-    Book(4,"HP1","Author 1","very nice book",2),
-    Book(5,"HP2","Author 2","very good book",3),
-    Book(6,"HP3","Author 3","Book description",1),
+    Book(1,"computer science pro","code with roby","very nice book",5,2024),
+    Book(2,"Be fast with fastapi","code with roby","this is great book",5,2000),
+    Book(3,"Master end point","code with roby","very nice book",4,2022),
+    Book(4,"HP1","Author 1","very nice book",2,2024),
+    Book(5,"HP2","Author 2","very good book",3,2025),
+    Book(6,"HP3","Author 3","Book description",1,2000),
 ]
 
-@app.get("/books")
+@app.get("/books",status_code=status.HTTP_200_OK)
 def read_all_books():
     return BOOKS
 
 
-@app.post("/create-book")
+@app.get("/books/{book_id}",status_code=status.HTTP_200_OK)
+def read_book(book_id:int=Path(gt=0)):
+    for book in BOOKS:
+        if book.id==book_id:
+            return book
+    raise HTTPException(status_code=404,detail="Item not found")
+
+
+        
+@app.get("/books/publish/",status_code=status.HTTP_200_OK)
+def filter_by_year(year:int=Query(gt=1999,lt=2031)):
+    book_to_return =[]
+    for book in BOOKS:
+        if book.published_date==year:
+            book_to_return.append(book)
+    return book_to_return
+
+
+
+@app.get("/books/",status_code=status.HTTP_200_OK)
+def read_book_by_rating(book_rating:int=Query(gt=0,lt=6)):
+    book_return=[]
+    for book in BOOKS:
+        if book.rating==book_rating:
+            book_return.append(book)
+    return book_return
+
+
+@app.post("/create-book",status_code=status.HTTP_201_CREATED)
 def create_book(book_request:BookRequest):
     new_book=Book(**book_request.model_dump())
     BOOKS.append(find_book_id(new_book))
+
 
 def find_book_id(book:Book):
     if len(BOOKS)>0:
@@ -64,4 +98,25 @@ def find_book_id(book:Book):
     
     return book
 
+@app.put("/books/update_book",status_code=status.HTTP_204_NO_CONTENT)
+def update_book(book:BookRequest):
+    book_changed=False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id==book.id:
+            BOOKS[i]=book
+    if not book_changed: 
+        raise HTTPException(status_code=404,detail="Item not found")
+
+
+
+@app.delete("/books/{book_id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_book(book_id:int=Path(gt=0)):
+    book_changed=False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id==book_id:
+            BOOKS.pop(i)
+            book_changed=True
+            break
+    if not book_changed: 
+        raise HTTPException(status_code=404,detail="Item not found")
 
